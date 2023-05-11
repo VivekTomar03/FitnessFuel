aee3acf7c198832505e8c5a8481bf06b92e4e6d2
mdfaizan973 <faizan.md9735@gmail.com>
2023-05-11 23:00:00 +0530

    added footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./Pages/Navbar";
+import Footer from "./Pages/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Footer />
     </div>
   );
 }
