755b274cdacfc0c802c2e34af13fb9b2cb4b58de
mdfaizan973 <faizan.md9735@gmail.com>
2023-05-11 23:44:49 +0530

    able to make home page

--- frontend/src/Pages/Navbar.tsx
+++ frontend/src/Pages/Navbar.tsx
@@ -79,7 +79,7 @@ export default function Navbar() {
               </div>
             </div>
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-              <button
+              {/* <button
                 type="button"
                 className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
               >
@@ -98,7 +98,7 @@ export default function Navbar() {
                     d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
                   />
                 </svg>
-              </button>
+              </button> */}
 
               <div className="relative ml-3">
                 <div>
@@ -116,6 +116,7 @@ export default function Navbar() {
                       alt=""
                     />
                   </button>
+                  <span className="text-sky-400">faizan@gmail.com</span>
                 </div>
 
                 {/* <div
