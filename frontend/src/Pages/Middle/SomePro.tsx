8fecca151cf7214d2ab1159ddff055bdda758d07
mdfaizan973 <faizan.md9735@gmail.com>
2023-05-11 23:13:11 +0530

    added some thing

--- frontend/src/Pages/Middle/SomePro.tsx
+++ frontend/src/Pages/Middle/SomePro.tsx
@@ -51,6 +51,50 @@ export default function SomePro() {
           </div>
         </div>
       </div>
+      <div className="bg-white  sm:pb-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <h2 className="text-center text-4xl font-semibold leading-8 text-gray-900">
+            Workout
+          </h2>
+          <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+            <img
+              className="col-span-2 max-h-100 w-full object-contain lg:col-span-1"
+              src="https://d18zdz9g6n5za7.cloudfront.net/video/640/640-1345-pyramid-sets-for-defined-arms-99aa.jpg"
+              alt="Transistor"
+              width="158"
+              height="48"
+            />
+            <img
+              className="col-span-2 max-h-100 w-full object-contain lg:col-span-1"
+              src="https://d18zdz9g6n5za7.cloudfront.net/video/640/640-1242-kettlebell-strength-emoms-a89f.jpg"
+              alt="Reform"
+              width="158"
+              height="48"
+            />
+            <img
+              className="col-span-2 max-h-100 w-full object-contain lg:col-span-1"
+              src="https://d18zdz9g6n5za7.cloudfront.net/video/640/640-1338-slow-tempo-lower-body-strength-bdc3.jpg"
+              alt="Tuple"
+              width="158"
+              height="48"
+            />
+            <img
+              className="col-span-2 max-h-100 w-full object-contain sm:col-start-2 lg:col-span-1"
+              src="https://d18zdz9g6n5za7.cloudfront.net/video/640/640-1265-yoga-for-lower-back-pain-8226.jpg"
+              alt="SavvyCal"
+              width="158"
+              height="48"
+            />
+            <img
+              className="col-span-2 col-start-2 max-h-100 w-full object-contain sm:col-start-auto lg:col-span-1"
+              src="https://d18zdz9g6n5za7.cloudfront.net/video/640/640-1288-bored-easily-hiit-cardio-and-abs-9385.jpg"
+              alt="Statamic"
+              width="158"
+              height="48"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
