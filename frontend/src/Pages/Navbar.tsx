69b1994235e2aa8e4b46ccf672ea2fdb0856276a
mdfaizan973 <faizan.md9735@gmail.com>
2023-05-12 00:05:26 +0530

    added some banner2

--- frontend/src/Pages/Navbar.tsx
+++ frontend/src/Pages/Navbar.tsx
@@ -73,7 +73,7 @@ export default function Navbar() {
                     href="#"
                     className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                   >
-                    Project
+                    Workout
                   </a>
                 </div>
               </div>
