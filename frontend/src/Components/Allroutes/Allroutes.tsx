ce343cfc53f2d6b0223d458309d238817bc3e0e5
mdfaizan973 <faizan.md9735@gmail.com>
2023-05-12 15:26:17 +0530

    aa

--- frontend/src/Components/Allroutes/Allroutes.tsx
+++ frontend/src/Components/Allroutes/Allroutes.tsx
@@ -5,13 +5,10 @@ import Signup from "../../Pages/Signup";
 import Homeui from "../../Pages/Middle/Homeui";
 import PageNotF from "../../Pages/PageNotF";
 import Workout from "../../Pages/Workout";
-// import Navbar from "../../Pages/Navbar";
-// import Footer from "../../Pages/Footer";
 
 function Allroutes() {
   return (
     <div>
-      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<Homeui />} />
         <Route path="/mylogin" element={<Login />} />
@@ -19,7 +16,6 @@ function Allroutes() {
         <Route path="*" element={<PageNotF />}></Route>
         <Route path="/workout" element={<Workout />}></Route>
       </Routes>
-      {/* <Footer /> */}
     </div>
   );
 }
